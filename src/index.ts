deb8d17ff9c9515c6cefcb55d21c9d68c7dc30ab
eb-pagos <endrit.bytyqi@pagos.ai>
2024-09-10 13:33:08 +0200

    the other case

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,13 @@ export const getGOLBoard = (boardPositions: Life[]) => {
     
     const result = {};
      for (const item of boardPositions){
-        if(result[item.x]){
+        if(!result[item.x]){
             const set_ = new Set()
             set_.add(item.y)
             result[item.x] = set_;
+        }
+        else {
+            result[item.x].add(item.y)
         }
      }
     return result;
