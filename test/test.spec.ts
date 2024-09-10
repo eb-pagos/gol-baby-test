9ea4be9eba2c3ec82dd20c40ad38a5e19dd69a2e
Kirill Kaluga <kirill.kaluga.s@gmail.com>
2024-09-10 13:14:06 +0200

    add initial data

---
+++ test/test.spec.ts
@@ -0,0 +1,10 @@
+interface Data {
+    title: string;
+    value: number;
+}
+
+const data: Data[] = [
+    { title: 'still', value: 1 },
+    { title: 'blinker', value: 2 },
+    { title: 'glider', value: 3 },
+];
