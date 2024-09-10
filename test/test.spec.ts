4a043bb949f629d90df484253e39bca848dc499a
Kirill Kaluga <kirill.kaluga.s@gmail.com>
2024-09-10 13:41:30 +0200

    add expected

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -3,9 +3,21 @@ import { Data } from "../src/type";
 import { getGOLBoard } from "../src";
 
 const data: Data[] = [
-    { title: 'still', lifes: [{x: 1,y: 1}, {x: 1,y: 2}, {x: 2,y: 1}, {x: 2,y: 2}] },
-    { title: 'blinker', lifes: [{x: 1,y: 1}, {x: 1,y: 2}, {x: 2,y: 1}, {x: 2,y: 2}] },
-    { title: 'glider', lifes: [{x: 1,y: 1}, {x: 1,y: 2}, {x: 2,y: 1}, {x: 2,y: 2}] },
+    { 
+        title: 'still', 
+        lifes: [{x: 1,y: 1}, {x: 1,y: 2}, {x: 2,y: 1}, {x: 2,y: 2}], 
+        expected: [{x: 1,y: 1}, {x: 1,y: 2}, {x: 2,y: 1}, {x: 2,y: 2}]  
+    },
+    { 
+        title: 'blinker', 
+        lifes: [{x: 1,y: 1}, {x: 1,y: 2}, {x: 2,y: 1}, {x: 2,y: 2}],
+        expected: [{x: 1,y: 1}, {x: 2,y: 1}, {x: 3,y: 1}]
+    },
+    { 
+        title: 'glider', 
+        lifes: [{x: 1,y: 1}, {x: 1,y: 2}, {x: 2,y: 1}, {x: 2,y: 2}],
+        expected: [{x: 1,y: 1}, {x: 2,y: 2}, {x: 3,y: 1}, {x: 3,y: 2}, {x: 2,y: 3}]
+    },
 ];
 
 test.describe("Test", async () => {
